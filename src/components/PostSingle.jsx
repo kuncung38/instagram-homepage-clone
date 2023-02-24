7e4dbfd0ecfcb0f2468b27063ff8a6e988d1a110
Kuncung <rickowijaya@gmail.com>
2023-02-24 23:55:58 +0700

    fix like color

--- src/components/PostSingle.jsx
+++ src/components/PostSingle.jsx
@@ -27,15 +27,22 @@ const PostSingle = ({ post }) => {
 
             <div className="flex justify-between p-2">
                 <div className="flex gap-4">
-                    <HeartIcon
+                    <svg
                         onClick={(e) => dispatcher(likePost(post?.id))}
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill={post?.user_has_liked ? "red" : "none"}
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
                         className="w-7 h-7"
-                        style={{
-                            backgroundColor: post?.user_has_liked
-                                ? "red"
-                                : "transparent",
-                        }}
-                    />
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                        />
+                    </svg>
+
                     <ChatBubbleOvalLeftIcon className="w-7 h-7" />
                     <PaperAirplaneIcon className="w-7 h-7" />
                 </div>
